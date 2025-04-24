4ac7e01f8cf6d0ce36012ad00a16259058035547
Jinzhi Cao <scu.jinzhi@gmail.com>
2025-04-23 20:51:33 -0400

    update joinus page

--- src/components/Joinus.js
+++ src/components/Joinus.js
@@ -1,17 +1,13 @@
 import React from "react";
-import Navbar from "./Navbar"; // Import the Navbar component
-import Multiplelinebreaks from "./Mulitplelinebreaks";
+import Navbar from "./Navbar";
 import Chat from "./Chat";
 import Title from "./Title";
 import "./Joinus.css";
 
-
 function Joinus() {
-    return (
-      <div className="home-container">
-      <div className="Navbar">
-        <Navbar />
-      </div>
+  return (
+    <div className="home-container">
+      <Navbar />
       <div className="main-content">
         <div className="title">
           <Title text="Join Us" />
@@ -19,24 +15,30 @@ function Joinus() {
         <div className="joinus-container">
           <div className="knowmore-card">
             <div className="joinus-text">
-              Interested in what we do ?
-              <br/>
-              Do you find everyting you seen so far interesting!!!
-              <br/>
-              You can contribute to this work.
-              <br/>
-              if not contact professor Miodrag for more interesting projects.
-              <br/>
-              Dr.Miodrag Bolic Email: Miodrag.Bolic@uottawa.ca
-              <br/>
-              if you have any suggestions for the website you can contact our 
-              web developers. 
-              <br/>
-              Let's build and learn together!!!
-              <br/>
-              <br/>
-              <p className="joinus-slogan">
-                Find out what other researchers are up to and potential collaborators by using below chat.
+              <h2>Interested in what we do?</h2>
+              <p>
+                Do you find everything you've seen so far interesting? You can contribute to this work!
+              </p>
+              <p>
+                Not interested? Contact Professor Miodrag for more exciting projects:
+                <br />
+                <strong>Email:</strong> <a href="mailto:Miodrag.Bolic@uottawa.ca">Miodrag.Bolic@uottawa.ca</a>
+              </p>
+              <p>
+                Have suggestions for the website? Reach out to our web developers. Let's build and learn together!
+              </p>
+              <button
+                className="cta-button"
+                onClick={() => {
+                  window.location.href = "mailto:Miodrag.Bolic@uottawa.ca";
+                }}
+                >
+                Contact Us
+              </button>
+            </div>
+            <div className="joinus-slogan">
+              <p>
+                Find out what other researchers are up to and potential collaborators by using the chat below.
               </p>
             </div>
             <Chat />
@@ -44,37 +46,7 @@ function Joinus() {
         </div>
       </div>
     </div>
+  );
+}
 
-      // <div>
-      //   <Navbar /> {/* Include the Navbar component */}
-      //   <Multiplelinebreaks count={4} />
-      //   <div className="Joinus-container">
-      //     <div className="knowmore-card">
-      //       <h1>Interested in what we do ??</h1>
-      //       <p>
-      //       Do you find everyting you seen so far interesting!!!
-      //       <br/>
-      //           You can contribute to this work.
-      //           <br/>
-      //           if not contact professor Miodrag for more interesting projects.
-      //           <br/>
-      //           Dr.Miodrag Bolic Email: Miodrag.Bolic@uottawa.ca
-      //           <br/>
-      //           if you have any suggestions for the website you can contact our 
-      //           web developers. 
-      //           <br/>
-      //           Let's build and learn together!!!
-      //           <br/>
-      //           <br/>
-      //           <p className="small-bold-text">
-      //             Find out what other researchers are up to and potential collaborators by using below chat.
-      //           </p>
-      //       </p>
-      //       <Chat />
-      //     </div>
-      //   </div>
-      // </div>
-    );
-  }
-  
-  export default Joinus;
+export default Joinus;
